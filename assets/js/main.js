159876f56dae9a935fc300b39e14daf4b42c54e2
Yevhenii <zhenyaliho94@gmail.com.com>
2021-01-27 23:40:14 +0200

    start

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+jQuery(document).ready(function ($) {
+  //Mob-menu
+  $('#header-burger').on('click', function (e) {
+    $(this).toggleClass('active');
+    $('#mobMenu').toggleClass('active');
+    $('body').toggleClass('overflow-h');
+  });
+  $('.close-mob-menu-js').on('click', function (e) {
+    $('body').removeClass('overflow-h');
+    $('#header-burger').removeClass('active');
+    $('#mobMenu').removeClass('active');
+  });
+
+  //=================== scroll to page
+  $('.scrollto').on('click', function () {
+    let href = $(this).attr('href');
+
+    $('html, body').animate(
+      {
+        scrollTop: $(href).offset().top,
+      },
+      {
+        duration: 370, // по умолчанию «400»
+        easing: 'linear', // по умолчанию «swing»
+      }
+    );
+    if ($(window).width() < 768) {
+      $('html, body').animate({
+        scrollTop: $(href).offset().top,
+      });
+    }
+    return false;
+  });
+  // ======================== MASK
+  $('.mask-phone').mask('+7 (999) 999-99-99');
+
+  $('#client-phone').on('blur input', function () {
+    if ($(this).val().length >= 18) {
+      $(this).closest('.form-input__wrapp').removeClass('--error');
+    } else {
+      $(this).closest('.form-input__wrapp').addClass('--error');
+    }
+  });
+
+  $('#form-action').submit(function (e) {
+    e.preventDefault();
+    $(this).hide();
+    $('#form-thank').show();
+  });
+
+  //scroll-header-menu
+  var scrollPos = 0;
+  $(document).scroll(function () {
+    if (300 < $(document).scrollTop()) {
+      $('#header').addClass('h-fixed');
+    } else {
+      $('#header').removeClass('h-fixed');
+    }
+
+    var st = $(this).scrollTop();
+    if (st < scrollPos) {
+      $('#header').removeClass('active');
+    } else {
+      $('#header').addClass('active');
+    }
+    scrollPos = st;
+    if (scrollPos <= 200) {
+      $('#header').removeClass('active');
+    }
+  });
+
+  //======================== SLICK SLIDERS
+  //===========================  front-page Slider
+
+  $('#panel-left-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '#panel-slider',
+  });
+
+  $('#panel-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    prevArrow: '<button class="slider-btn slider-btn__prev"><span></span></button>',
+    nextArrow: '<button class="slider-btn slider-btn__next"><span></span></button>',
+    fade: true,
+    dots: true,
+    infinite: false,
+    asNavFor: '#panel-left-slider',
+  });
+
+  //Wow
+  new WOW().init();
+});
